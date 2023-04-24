cfc09f03579a57107180c7e475ebea2714a2e045
DESKTOP-R2NP3JG\user <wayne30317@gmail.com>
2023-04-24 19:26:23 +0800

    環境建置

---
+++ personalWebsite/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>hi</h1>
+</template>
+
+<style lang="scss">
+    *{
+        border: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+</style>
